2eff1bc75930c3abf513a1a2935e58ccff29db72
m-zaremba <zaremba_michal@wp.pl>
2019-10-11 16:49:58 +0200

    Add Project update

--- src/components/AddProject.js
+++ src/components/AddProject.js
@@ -1,7 +1,70 @@
-import React from 'react';
+import React, {useState} from 'react';
+import { firebase } from '../firebase';
+import { generatePushId } from '../helpers';
+import { useProjectsValue } from '../context';
 
-export const AddProject = () => {
+export const AddProject = ({shouldShow = false}) => {
+  const [show, setShow] = useState(shouldShow);
+  const [projectName, setProjectName] = useState('');
+
+  const projectId = generatePushId();
+  const {setProjects} = useProjectsValue();
+
+  const addProject = () =>
+    projectName &&
+    firebase
+      .firestore()
+      .collection('projects')
+      .add({
+        projectId,
+        name: projectName,
+        userId: 'testUserForTheAppDevelopment'
+      })
+      .then(() => {
+        setProjects([]);
+        setProjectName('');
+        setShow(false);
+      });
+
   return (
-    <p>Hello Add Project!</p>
-  )
-}
+    <div className="add-project" data-testid="add-project">
+      {show && (
+        <div className="add-project__input" data-testid="add-project-inner">
+          <input
+            value={projectName}
+            onChange={e => setProjectName(e.target.value)}
+            className="add-project__name"
+            data-testid="project-name"
+            type="text"
+            placeholder="Name your project"
+          />
+          <button
+            className="add-project__submit"
+            type="button"
+            onClick={() => addProject()}
+            data-testid="add-project-submit"
+          >
+            Add Project
+          </button>
+          <span
+            aria-label="Cancel adding project"
+            data-testid="hide-project-overlay"
+            className="add-project__cancel"
+            onClick={() => setShow(false)}
+          >
+            Cancel
+          </span>
+        </div>
+      )}
+      <span className="add-project__plus">+</span>
+      <span
+        aria-label="Add Project"
+        data-testid="add-project-action"
+        className="add-project__text"
+        onClick={() => setShow(!show)}
+      >
+        Add Project
+      </span>
+    </div>
+  );
+};
