965588bd9ad147ff7faea401524f7281d6f8754d
m-zaremba <zaremba_michal@wp.pl>
2019-10-16 20:04:28 +0200

    Project Overlay component added

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -3,6 +3,7 @@ import {FaRegListAlt, FaRegCalendarAlt} from 'react-icons/fa';
 import moment from 'moment';
 import {firebase} from '../firebase';
 import {useSelectedProjectValue} from '../context';
+import {ProjectOverlay} from './ProjectOverlay';
 
 export const AddTask = ({
   showAddTaskMain = true,
@@ -83,7 +84,10 @@ export const AddTask = ({
               </div>
             </>
           )}
-          <p>Project overlay here</p>
+          <ProjectOverlay
+            setProject={setProject}
+            showProjectOverlay={showProjectOverlay}
+            setShowProjectOverlay={setShowProjectOverlay}/>
           <p>TaskDate here</p>
           <input
             type="text"
